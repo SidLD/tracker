253373d06e8840c912b004b19216191572352d1a
= <cidlibril5@gmail.com>
2024-08-13 22:29:17 +0800

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+import { LatestPost } from "@/app/_components/post";
+import { getServerAuthSession } from "@/server/auth";
+import { api, HydrateClient } from "@/trpc/server";
+import { LoginForm } from "./_components/login/_components/login";
+
+export default async function Home() {
+
+  return (
+    <HydrateClient>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+            <LoginForm />
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
